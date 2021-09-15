a2a63c3774d5ebbcf97a76d4ee514ba5d1a9e041
Momchil Marinov <mom4ilakis@gmail.com>
2021-09-15 17:46:37 +0300

    Add user in voice check

--- commands/play.js
+++ commands/play.js
@@ -17,15 +17,23 @@ module.exports = {
         .addStringOption(option => option.setName('url').setDescription('Youtube video url.')),
 
     async execute(message) {
-        console.log(message.member.guild.id)
-        console.log(message.member.voice.channel.id)
+        const { voice, guild } =  message.member
+
+        console.log(guild.id)
+        console.log(voice.channel.id)
         console.log(message.options.getString('url'))
+
+
+        if (!voice) {
+            return message.reply({content: 'You need to be in a voice channel', ephemeral: true})
+        }
+
         const connection = joinVoiceChannel({
-            channelId: message.member.voice.channel.id,
-            guildId: message.member.guild.id,
+            channelId: voice.channel.id,
+            guildId: guild.id,
             adapterCreator: message.member.guild.voiceAdapterCreator,
         });
-        
+
         const stream = ytdl(message.options.getString('url'), { filter: 'audioonly' });
         const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
         const player = createAudioPlayer();
@@ -37,14 +45,15 @@ module.exports = {
             console.error(`Exception: ${error.message}`)
             player.stop(true)
         })
-        
+
         connection.on(VoiceConnectionStatus.Disconnected, () => {
             console.log('destroying connection')
             return player.stop(true)
         })
-        
+
+        player.on('finish', () => console.log('finished playing'))
         player.on(AudioPlayerStatus.Idle, () => connection.destroy());
-        
+
         return message.reply('Playing')
     }
 }
